3e0210700b6623948a02355d65165147b58b5518
Naresh Adhe <nareshadhe01@gmail.com>
2026-05-22 23:16:45 +0530

    Added the multi-stage pipeline support

--- main.cpp
+++ main.cpp
@@ -58,61 +58,114 @@ int main()
 
         int size = tokens.size();
 
-        int pipeIndex = -1;
+        bool backgroundProcess = false;
+
+        if (tokens.back() == "&")
+        {
+            backgroundProcess = true;
+
+            tokens.pop_back();
+
+            size = tokens.size();
+        }
+
+        vector<int> pipeIndices;
         for (int i = 0; i < size; i++)
         {
             if (tokens[i] == "|")
             {
-                pipeIndex = i;
-                break;
+                pipeIndices.push_back(i);
             }
         }
 
-        if (pipeIndex == 0 || pipeIndex == tokens.size() - 1)
+        int pipeSize = pipeIndices.size();
+
+        for (int i = 1; i < pipeSize; i++)
+        {
+            if (pipeIndices[i] - pipeIndices[i - 1] == 1)
+            {
+                cout << "Invalid pipe command" << endl;
+                continue;
+            }
+        }
+
+        if (pipeIndices.front() == 0 || pipeIndices.back() == tokens.size() - 1)
         {
             cout << "Invalid pipe command" << endl;
             continue;
         }
 
-        if (pipeIndex != -1)
+        if (pipeSize > 0)
         {
-            vector<string> leftTokens, rightTokens;
+            vector<vector<string>> commands(pipeSize + 1);
 
-            getCommand(0, pipeIndex, leftTokens, tokens);
-            getCommand(pipeIndex + 1, size, rightTokens, tokens);
-
-            vector<char *> leftCommand, rightCommand;
-            
-            convertToCharPointerArray(leftCommand, leftTokens);
-            convertToCharPointerArray(rightCommand, rightTokens);
-
-            int fd[2];
-            pipe(fd);
-
-            pid_t pid1 = fork();
-
-            if (pid1 == 0)
+            for (int i = 0; i <= pipeSize; i++)
             {
-                close(fd[0]);
-                dup2(fd[1], STDOUT_FILENO);
-                close(fd[1]);
-                execvp(leftCommand[0], leftCommand.data());
+                if (i == 0)
+                {
+                    getCommand(0, pipeIndices[i], commands[i], tokens);
+                }
+                else
+                {
+                    getCommand(pipeIndices[i - 1] + 1, pipeIndices[i], commands[i], tokens);
+                }
             }
 
-            pid_t pid2 = fork();
-            if (pid2 == 0)
+            getCommand(pipeIndices[pipeSize - 1] + 1, size, commands[pipeSize], tokens);
+
+            int prevPipeRead = -1;
+
+            for (int i = 0; i <= pipeSize; i++)
             {
-                close(fd[1]);
-                dup2(fd[0], STDIN_FILENO);
-                close(fd[0]);
-                execvp(rightCommand[0], rightCommand.data());
+
+                vector<char *> args;
+
+                convertToCharPointerArray(args, commands[i]);
+
+                int fd[2];
+                if (i != pipeSize)
+                {
+                    pipe(fd);
+                }
+
+                pid_t pid = fork();
+
+                if (pid == 0)
+                {
+                    if (prevPipeRead != -1)
+                    {
+                        dup2(prevPipeRead, STDIN_FILENO);
+                        close(prevPipeRead);
+                    }
+
+                    if (i != pipeSize)
+                    {
+                        dup2(fd[1], STDOUT_FILENO);
+                        close(fd[1]);
+                    }
+
+                    if (i != pipeSize)
+                    {
+                        close(fd[0]);
+                        close(fd[1]);
+                    }
+
+                    execvp(args[0], args.data());
+
+                    perror("execvp failed");
+                    exit(1);
+                }
+                if(i != pipeSize){
+                    close(fd[1]);
+                    if(prevPipeRead != -1) close(prevPipeRead);
+                    prevPipeRead = fd[0];
+                }
             }
 
-            close(fd[0]);
-            close(fd[1]);
-
-            wait(NULL);
-            wait(NULL);
+            for (int i = 0; i <= pipeSize; i++)
+            {
+                wait(NULL);
+            }
 
             continue;
         }
@@ -138,8 +191,13 @@ int main()
             execvp(args[0], args.data());
 
             perror("execvp failed");
+
+            exit(1);
         }
 
-        wait(NULL);
+        if (backgroundProcess == false)
+        {
+            wait(NULL);
+        }
     }
 }
